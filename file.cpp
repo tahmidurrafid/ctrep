5367be8c64f00ab266917e9e5a6d60b22c3b3f0f
tahmidurrafid <amtrafid@gmail.com>
2021-01-16 09:24:42 +0600

    update

--- file.cpp
+++ file.cpp
@@ -1,5 +1,37 @@
 #include<stdio.h>
 
+int visited[500][500];
+
+char mat[500][500];
+int row, col;
+
+
+
+void dfs(int i, int j){
+    int x, y, p, q;
+    visited[i][j] = 1;
+
+    for(x = -1; x <= 1; x++){
+        for(y = -1; y <= 1; y++){
+            if(x == y) continue;
+            p = i + x;
+            q = j + y;
+            if(i + x < 0 || i + x >= row || j + y < 0 || j + y >= col )
+                continue;
+            if(visited[i+x][j+y] || mat[i+x][j+y] == '-' || mat[i+x][j+y] == 'X' ){
+                continue;
+            }
+            dfs(p, q);
+        }
+    }
+}
+
 int main(){
-    printf("Hello world\n");
+    int i, j, k;
+    scanf("%d%d", &row, &col);
+    for(i = 0; i < row; i++){
+        scanf("%s", mat[i]);
+    }
+    printf("%d%d", row, col);
+
 }
